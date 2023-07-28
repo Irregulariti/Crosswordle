90a28b7647de9dabc9ab5ab369c69b118adaf100
Irregulariti <ermolin.aa@edu.spbstu.ru>
2023-07-28 15:48:57 +0300

    fix bottom nav bar add flags

--- app/src/main/java/com/irregulariti/crosswordle/MainActivity.kt
+++ app/src/main/java/com/irregulariti/crosswordle/MainActivity.kt
@@ -9,30 +9,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.irregulariti.crosswordle.screenparams.TransparentSystemBars
 import com.irregulariti.crosswordle.ui.bottomnavigation.ui.CreateBar
 import com.irregulariti.crosswordle.ui.theme.MyApplicationTheme
 
-@Suppress("DEPRECATION")
-class MainActivity : ComponentActivity() {
-    override fun onResume() {
-        super.onResume()
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-    }
 
-    override fun onStart() {
-        super.onStart()
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-    }
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val systemUiController: SystemUiController = rememberSystemUiController()
 
-            systemUiController.isStatusBarVisible = false // Status bar
-            systemUiController.isNavigationBarVisible = false // Navigation bar
-            systemUiController.isSystemBarsVisible = false
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            TransparentSystemBars()
             CreateBar()
         }
     }
