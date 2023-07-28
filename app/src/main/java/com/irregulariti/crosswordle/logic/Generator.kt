d31b2826f8eea307ba27e634beb9f651a3a1a954
Veles <tema@40ino.com>
2023-07-28 13:50:09 +0300

    1

--- app/src/main/java/com/irregulariti/crosswordle/logic/Generator.kt
+++ app/src/main/java/com/irregulariti/crosswordle/logic/Generator.kt
@@ -1,2 +1,27 @@
 package com.irregulariti.crosswordle.logic
 
+fun Generate(Len: Int, Deep: Int){
+
+    val Words = setOf<String>()
+
+    /*
+    1) выбрать длинное слово
+    2) выбрать 2-5 рандом соседних букв
+    3)
+     */
+}
+
+fun Size(i: Int): Int{
+    //возвращает массив слов опред длинны
+    return 5
+}
+
+fun Bukova(b: String): Int {
+    //возвращает массив слов с опред буковй
+    return 9
+}
+
+fun Bukov(a: String, b: String, i: Int): Int {
+    //возвращает массив слов с опред буквами на опред расстоянии друг от друга
+    return 13
+}
